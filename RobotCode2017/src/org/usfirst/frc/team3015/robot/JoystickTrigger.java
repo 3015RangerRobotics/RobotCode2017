dcd2a4fdd4e56548104f31d33a346be004e7b4ad
HaiYen <haiyen2001510@yahoo.com>
2017-01-16 20:50:18 -0500

    class for Joystick Triggers to be true/false

--- RobotCode2017/src/org/usfirst/frc/team3015/robot/JoystickTrigger.java
+++ RobotCode2017/src/org/usfirst/frc/team3015/robot/JoystickTrigger.java
@@ -1,13 +1,21 @@
 package org.usfirst.frc.team3015.robot;
 //import org.usfirst.frc.team3015.
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 public class JoystickTrigger extends Button {
-
+	private Joystick m_joystick; 
+	private int m_triggerAxis;
+	private double m_triggerDistance;
+	public JoystickTrigger (Joystick joystick,int triggerAxis, double triggerDistance) {
+		m_joystick=joystick;
+		m_triggerAxis=triggerAxis;
+		m_triggerDistance=triggerDistance;
+	}
 	@Override
 	public boolean get() {
-		return false;
+		return (m_joystick.getRawAxis(m_triggerAxis)>=m_triggerDistance);
 	}
 
 }

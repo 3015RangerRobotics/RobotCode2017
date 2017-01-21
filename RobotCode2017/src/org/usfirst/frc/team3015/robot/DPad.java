ee0f29550740cf47ecd408e995c5074b37ba402a
Keshawn <keshawnprimoussherman@yahoo.com>
2017-01-21 10:43:07 -0500

    stuff

--- RobotCode2017/src/org/usfirst/frc/team3015/robot/DPad.java
+++ RobotCode2017/src/org/usfirst/frc/team3015/robot/DPad.java
@@ -3,10 +3,21 @@ package org.usfirst.frc.team3015.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
+/**
+ * Takes a DPad input direction and converts it to a Button for Commands
+ * 
+ * @author rangerrobots
+ *
+ */
 public class DPad extends Button{
 	private int dPadDegree;
 	private Joystick joystick;
 	
+	/**
+	 * DPad directions in words
+	 * @author rangerrobots
+	 *
+	 */
 	public enum Value {
 		kDPadRight,
 		kDPadUpRight,
@@ -18,8 +29,16 @@ public class DPad extends Button{
 		kDPadDownRight	
 	}
 	
+	/**
+	 * DPad constructor based on enums
+	 * 
+	 * @param joystick 		The gamepad we are referencing
+	 * @param value 		uses enumeration names to select the 
+	 * 						DPad direction
+	 */
 	public DPad(Joystick joystick, Value value){
 		this.joystick = joystick;
+		//pass the dpad direction in degrees, based on the enum
 		switch (value){
 			case kDPadRight:
 				this.dPadDegree = 0;
@@ -50,11 +69,20 @@ public class DPad extends Button{
 		}
 	}
 	
+	/**
+	 * Simple DPad constructor
+	 * 
+	 * @param joystick 		The gamepad we are referencing
+	 * @param dPadDegree 	the direction on the DPad in degrees
+	 */
 	public DPad(Joystick joystick, int dPadDegree){
 		this.dPadDegree = dPadDegree;
 		this.joystick = joystick;
 	}
 
+	/**
+	 * outputs true if joystick POV matches specified DPad degree
+	 */
 	@Override
 	public boolean get() {
 		return (joystick.getPOV() == dPadDegree);

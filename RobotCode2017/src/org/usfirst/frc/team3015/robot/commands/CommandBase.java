4f80dd58340715b2672c9bfa2d4237e9afc8b03f
HaiYen+NickG <haiyen2001510@yahoo.com>
2017-01-21 13:33:25 -0500

    Climber subsystem

--- RobotCode2017/src/org/usfirst/frc/team3015/robot/commands/CommandBase.java
+++ RobotCode2017/src/org/usfirst/frc/team3015/robot/commands/CommandBase.java
@@ -12,6 +12,7 @@ import org.usfirst.frc.team3015.robot.subsystems.*;
 public abstract class CommandBase extends Command {
 	public static DriveTrain drive;
 	public static Shooter shooter;
+	public static Climber climber;
 	public static Vision vision;
 	public static OI oi;
 	
@@ -19,6 +20,7 @@ public abstract class CommandBase extends Command {
 		drive = new DriveTrain();
 		shooter = new Shooter();
 		vision = new Vision();
+		climber = new Climber();
 		oi = new OI();
 	}
 }

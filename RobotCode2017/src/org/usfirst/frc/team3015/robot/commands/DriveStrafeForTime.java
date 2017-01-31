2ece1ce69b6c5ada232f9f3586d8f7c95d7ab4aa
keshawn <keshawnprimoussherman@yahoo.com>
2017-01-30 19:06:27 -0500

    ayo im keshawn and i love doing the DriveStrafeForTime command
    matt did it but its in my name
    
    matt also typed this message

---
+++ RobotCode2017/src/org/usfirst/frc/team3015/robot/commands/DriveStrafeForTime.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team3015.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveStrafeForTime extends CommandBase {
+	private double speed;
+	
+    public DriveStrafeForTime(double time, double speed) {
+        this.speed = speed; 
+        this.setTimeout(time);
+        requires(drive);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	drive.hDrive(speed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	drive.hDrive(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}

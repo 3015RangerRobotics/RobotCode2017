cacd19da5770e168267402c01879bea2e450747d
Michael <mjansen4857@gmail.com>
2017-02-13 21:03:44 -0500

    ew

--- RobotCode2017/src/org/usfirst/frc/team3015/robot/subsystems/CameraStream.java
+++ RobotCode2017/src/org/usfirst/frc/team3015/robot/subsystems/CameraStream.java
@@ -1,8 +1,12 @@
 package org.usfirst.frc.team3015.robot.subsystems;
 
+import org.usfirst.frc.team3015.robot.commands.StreamFrontCamera;
+
 import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -13,21 +17,39 @@ public class CameraStream extends Subsystem {
 	MjpegServer server;
 	
 	public CameraStream(){
-		frontCamera = new UsbCamera("Front", 0);
-		backCamera = new UsbCamera("Back", 1);
-		server = new MjpegServer("Server", 1181);
+//		frontCamera = new UsbCamera("USB Camera 0", 0);
+//		backCamera = new UsbCamera("Back", 1);
+//		server = new MjpegServer("Server", 1180);
+//		server.setSource(frontCamera);
+		frontCamera = CameraServer.getInstance().startAutomaticCapture(0);
+		frontCamera.setResolution(854, 480);
+		backCamera = CameraServer.getInstance().startAutomaticCapture(1);
+		backCamera.setResolution(854, 480);
+		server = CameraServer.getInstance().addServer("cameraStream", 1180);
+		server.setSource(frontCamera);
+		SmartDashboard.putBoolean("isFront", true);
 	}
 
     public void initDefaultCommand() {
-    	
+//    	this.setDefaultCommand(new StreamFrontCamera());
     }
     
     public void streamFront(){
+//    	frontCamera = CameraServer.getInstance().startAutomaticCapture(0);
+//		frontCamera.setResolution(854, 480);
+//		server = CameraServer.getInstance().addServer("cameraStream", 1180);
     	server.setSource(frontCamera);
+    	SmartDashboard.putBoolean("refreshStream", true);
+    	SmartDashboard.putBoolean("isFront", true);
     }
     
     public void streamBack(){
+//    	backCamera = CameraServer.getInstance().startAutomaticCapture(1);
+//		backCamera.setResolution(854, 480);
+//		server = CameraServer.getInstance().addServer("cameraStream", 1180);
     	server.setSource(backCamera);
+    	SmartDashboard.putBoolean("refreshStream", true);
+    	SmartDashboard.putBoolean("isFront", false);
     }
 }
 

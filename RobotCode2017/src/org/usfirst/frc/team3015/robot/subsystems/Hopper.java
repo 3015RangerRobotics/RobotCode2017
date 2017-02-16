2f5e0ac270b559cc0fa61d1eb1a887736df8688d
Keshawn <keshawnprimoussherman@yahoo.com>
2017-02-15 20:43:16 -0500

    Multiplied values of the harvester by the voltage

--- RobotCode2017/src/org/usfirst/frc/team3015/robot/subsystems/Hopper.java
+++ RobotCode2017/src/org/usfirst/frc/team3015/robot/subsystems/Hopper.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3015.robot.subsystems;
 import org.usfirst.frc.team3015.robot.commands.HopperStop;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.ControllerPower;
 import edu.wpi.first.wpilibj.VictorSP;
@@ -16,14 +17,17 @@ public class Hopper extends Subsystem {
 
     private CANTalon rotation;
     private VictorSP ballFeeder;
-    private final double ROTATION_SPEED = -0.9;
-    private final double REVERSE_ROTATION_SPEED = 0.35;
+    private final double ROTATION_SPEED = -0.9*12.25;
+    private final double REVERSE_ROTATION_SPEED = 0.35*12.25;
+    private final double START_BALLFEEDER_SPEED = -.9*12.25;
+    private final double REVERSE_BALLFEEDER_SPEED = 0.8*12.25;
     
     /**
      * Constructs hardware
      */
     public Hopper() {
     	rotation = new CANTalon(2);
+    	rotation.changeControlMode(TalonControlMode.Voltage);
 		ballFeeder = new VictorSP(6);
     }
     
@@ -48,11 +52,11 @@ public class Hopper extends Subsystem {
     }
     
     public void startFeeder(){
-    	ballFeeder.set( -1);
+    	ballFeeder.set(START_BALLFEEDER_SPEED/ControllerPower.getInputVoltage());
     }
     
     public void reverseFeeder(){
-    	ballFeeder.set(0.8);
+    	ballFeeder.set(REVERSE_BALLFEEDER_SPEED/ControllerPower.getInputVoltage());
     }
     
     public void stopFeeder(){
